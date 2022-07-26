ab7a3321d53bb6c7d7c6a37fb24b14234769cb18
Sofía Mora Muñoz <sofiamoramunoz6@gmail.com>
2022-07-26 17:14:06 -0600

    Update Test-1

--- Simulación/Test-1.py
+++ Simulación/Test-1.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 browser = webdriver.Chrome('/Users/sofiamora/chromedriver/chromedriver')
 browser.get('https://www.demoblaze.com/index.html')
@@ -10,4 +12,9 @@ browser.get('https://www.demoblaze.com/prod.html?idp_=1')
 product_btn = browser.find_element(By.CSS_SELECTOR, 'a.btn-success')
 product_btn.click()
 
+WebDriverWait(browser, 10).until(EC.alert_is_present())
+browser.switch_to.alert.accept()
+
 browser.get('https://www.demoblaze.com/cart.html')
+product_btn = browser.find_element(By.CSS_SELECTOR, 'button.btn-success')
+
